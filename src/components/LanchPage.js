c414fd150924dbe390c06b465319623617899b4b
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-09-09 19:29:26 -0500

    svg background, navbar

--- src/components/LanchPage.js
+++ src/components/LanchPage.js
@@ -1,48 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faPython, faJs }  from '@fortawesome/free-brands-svg-icons';
+import { faReact, faPython, faJs } from '@fortawesome/free-brands-svg-icons';
 
 const LanchPage = () => {
     return (
 
-        <div id="bg">
-            <header>
-                <img src="images/felipelogo.png" alt="Logo" />
-                <a href="#">Felipe Cantu Jr</a>
-            </header>
 
-            <main>
-                <section id="card">
-                    <ul>
-                        <li>
-                            <span>
-                                <FontAwesomeIcon icon={faReact} />
-                                <strong>Front-End Developer</strong>
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                <FontAwesomeIcon icon={faPython} />
-                                <strong>Back-End Developer</strong>
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                <FontAwesomeIcon icon={faJs} />
-                                <strong>Lets make some things happen</strong>
-                            </span>
-                        </li>
-                    </ul>
-                </section>
-                <section id="primary">
-                    <h1>Hello, My Name is Felipe Cantu Jr.</h1>
-                    <p>I am Your Full Stack Developer</p>
 
-                    <a href="#">My Work</a>
-                </section>
-            </main>
-
-        </div>
+        <svg id="bg" class="bg-teal-700 h-screen">
+            <path class=" clip-path: polygon(0 0, 80% 0%, 100% 0, 100% 24%, 54% 100%, 54% 100%, 0 70%, 0% 20%) "/>
+        </svg>
 
     )
 }
